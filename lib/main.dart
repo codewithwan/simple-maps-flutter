017a21432ba97af04c57e9d4da2a2e549c9c94bc
Muhammad Ridwan <hhhh25737@gmail.com>
2024-12-10 07:56:07 +0700

    chore: move home_screen to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:maps_flutter/widgets/primary_app_bar.dart';
+import 'screens/maps_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,6 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Maps',
       home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PrimaryAppBar(title: 'Simple Maps'),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MapsScreen()),
+            );
+          },
+          child: const Text('Show Maps'),
+        ),
+      ),
     );
   }
 }
